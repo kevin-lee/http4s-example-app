cb2d773bb0e6af61b9587ad8ee0695da5e7d6418
Kevin Lee <kevin.github@kevinlee.io>
2021-12-10 14:36:00 +0000

    Update scalacheck to 1.15.4

--- project/Version.scala
+++ project/Version.scala
@@ -6,5 +6,5 @@ object Version {
   val Http4sVersion = "0.18.9"
   val LogbackVersion = "1.2.7"
   val Specs2Version = "4.13.1"
-  val ScalaCheckVersion = "1.14.3"
+  val ScalaCheckVersion = "1.15.4"
 }
