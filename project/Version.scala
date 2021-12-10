dc78d74406e0ad6574e8919f284900cf38e6363e
Kevin Lee <kevin.github@kevinlee.io>
2021-12-10 14:35:52 +0000

    Update http4s-blaze-server, http4s-circe, ... to 0.18.26

--- project/Version.scala
+++ project/Version.scala
@@ -3,7 +3,7 @@
   * @since 2018-05-12
   */
 object Version {
-  val Http4sVersion = "0.18.9"
+  val Http4sVersion = "0.18.26"
   val LogbackVersion = "1.2.7"
   val Specs2Version = "4.13.1"
   val ScalaCheckVersion = "1.14.3"
