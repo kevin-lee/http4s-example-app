d09c86dafb447758ab338815645d8211acfdd387
Kevin Lee <kevin.code@kevinlee.io>
2021-12-11 19:04:34 +1100

    Remove: unused library info

--- project/Version.scala
+++
@@ -1,9 +0,0 @@
-/** @author Kevin Lee
-  * @since 2018-05-12
-  */
-object Version {
-  val Http4sVersion     = "0.22.8"
-  val LogbackVersion    = "1.2.7"
-  val Specs2Version     = "4.13.1"
-  val ScalaCheckVersion = "1.14.3"
-}
