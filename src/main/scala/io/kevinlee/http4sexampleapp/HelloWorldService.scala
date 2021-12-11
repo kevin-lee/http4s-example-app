fdf479c6bd4121cb534efd8bccf6784d9fd0750d
Kevin Lee <kevin.code@kevinlee.io>
2021-12-11 18:51:23 +1100

    Upgrade: http4s 0.18.26 => 0.22.8 and replace specs2 and scalacheck with hedgehog

--- src/main/scala/io/kevinlee/http4sexampleapp/HelloWorldService.scala
+++ src/main/scala/io/kevinlee/http4sexampleapp/HelloWorldService.scala
@@ -1,27 +1,24 @@
 package io.kevinlee.http4sexampleapp
 
-import cats.effect.IO
+import cats.effect._
 import io.circe.Json
-import org.http4s.HttpService
+import org.http4s.HttpRoutes
 import org.http4s.circe._
 import org.http4s.dsl.Http4sDsl
 
-object HelloWorldService extends Http4sDsl[IO] {
+object HelloWorldService {
 
-  val service: HttpService[IO] = HttpService[IO] {
-    case GET -> Root =>
-      Ok(
-        Json.obj("message" -> Json.fromString("Hello, World"))
-      )
+  def service[F[_]: Sync](implicit dsl: Http4sDsl[F]): HttpRoutes[F] = {
+    import dsl._
+    HttpRoutes.of[F] {
+      case GET -> Root =>
+        Ok(Json.obj("message" -> Json.fromString("Hello, World")))
 
-    case GET -> Root / name =>
-      Ok(
-        Json.obj("message" -> Json.fromString(s"Hello, $name"))
-      )
+      case GET -> Root / name =>
+        Ok(Json.obj("message" -> Json.fromString(s"Hello, $name")))
 
-    case GET -> Root / "add" / IntVar(a) / IntVar(b) =>
-      Ok(
-        Json.obj("result" -> Json.fromLong(a.toLong + b.toLong))
-      )
+      case GET -> Root / "add" / IntVar(a) / IntVar(b) =>
+        Ok(Json.obj("result" -> Json.fromLong(a.toLong + b.toLong)))
+    }
   }
 }
